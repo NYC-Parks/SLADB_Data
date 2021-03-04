795c0562e055bf6a8878a64cb6579ca29222252e
SQL_Agent <sql_agent@nycdpr.parks.nycnet>
2021-03-04 14:17:50 -0500

    Added latest tbl_change_request data export to the csv.

--- export_vw_change_request_export.bat
+++ export_vw_change_request_export.bat
@@ -1,8 +1,12 @@
 @echo off
 
-sqlcmd -S . -E -i export_vw_change_request_export.sql -s "|" -o temp_tbl_change_request.csv -m 1 -W & find /V "---" < temp_tbl_change_request.csv > tbl_change_request.csv
+sqlcmd -S . -E -i %~dp0\export_vw_change_request_export.sql -s "|" -o %~dp0\temp_tbl_change_request.csv -m 1 -W & find /V "---" < %~dp0\temp_tbl_change_request.csv > %~dp0\tbl_change_request.csv
 
-del temp_tbl_change_request.csv
+del %~dp0\temp_tbl_change_request.csv
+
+cd /d %~d0
+
+cd %~dp0
 
 git add .
 
--- export_vw_change_request_status_export.bat
+++ export_vw_change_request_status_export.bat
@@ -1,8 +1,12 @@
 @echo off
 
-sqlcmd -S . -E -i export_vw_change_request_status_export.sql -s "|" -o temp_tbl_change_request_status.csv -m 1 -W & find /V "---" < temp_tbl_change_request_status.csv > tbl_change_request_status.csv
+sqlcmd -S . -E -i %~dp0\export_vw_change_request_status_export.sql -s "|" -o %~dp0\temp_tbl_change_request_status.csv -m 1 -W & find /V "---" < %~dp0\temp_tbl_change_request_status.csv > %~dp0\tbl_change_request_status.csv
 
-del temp_tbl_change_request_status.csv
+del %~dp0\temp_tbl_change_request_status.csv
+
+cd /d %~d0
+
+cd %~dp0
 
 git add .
 
