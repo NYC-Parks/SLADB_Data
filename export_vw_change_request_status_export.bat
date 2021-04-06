5856a027c2440167eff130847aa35adf0ace74f1
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2021-04-05 20:43:48 -0400

    Added latest tbl_change_request_status data export to the csv.

--- export_vw_change_request_status_export.bat
+++ export_vw_change_request_status_export.bat
@@ -1,8 +1,6 @@
 @echo off
 
-sqlcmd -S . -E -i %~dp0\export_vw_change_request_status_export.sql -s "|" -o %~dp0\temp_tbl_change_request_status.csv -m 1 -W & find /V "---" < %~dp0\temp_tbl_change_request_status.csv > %~dp0\tbl_change_request_status.csv
-
-del %~dp0\temp_tbl_change_request_status.csv
+bcp "use sladb open symmetric key sladb_symkey decryption by certificate sladb_cert; declare @json nvarchar(max) = (select change_request_status_id, change_request_id, sla_change_status, created_date_utc, created_user from sladb.dbo.vw_change_request_status_export for json path); if (isjson(@json) = 1) begin select @json as result end; close symmetric key sladb_symkey;" queryout tbl_change_request_status.json -T -c
 
 cd /d %~d0
 
@@ -12,6 +10,6 @@ git add .
 
 git commit -m "Added latest tbl_change_request_status data export to the csv."
 
-git push origin master
+REM git push origin master
 
-exit
+pause
