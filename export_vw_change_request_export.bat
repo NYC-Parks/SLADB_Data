25a2f3dfcbb9214f69121c346dec453cf2715a7d
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2021-04-05 20:39:55 -0400

    Added bcp script to export json, removed csv files.

--- export_vw_change_request_export.bat
+++ export_vw_change_request_export.bat
@@ -1,8 +1,7 @@
 @echo off
 
-sqlcmd -S . -E -i %~dp0\export_vw_change_request_export.sql -s "|" -o %~dp0\temp_tbl_change_request.csv -m 1 -W & find /V "---" < %~dp0\temp_tbl_change_request.csv > %~dp0\tbl_change_request.csv
-
-del %~dp0\temp_tbl_change_request.csv
+REM Use the BCP utility to export a json file with the change request data 
+bcp "use sladb open symmetric key sladb_symkey decryption by certificate sladb_cert; declare @json nvarchar(max) = (select change_request_id, unit_id, sla_code, season_id, effective_start, effective_start_adj, change_request_justification, change_request_comments, sla_change_status, edited_user from sladb.dbo.vw_change_request_export for json path); if (isjson(@json) = 1) begin select @json as result end; close symmetric key sladb_symkey;" queryout tbl_change_request.json -T -c
 
 cd /d %~d0
 
@@ -10,9 +9,8 @@ cd %~dp0
 
 git add .
 
-git commit -m "Added latest tbl_change_request data export to the csv."
+git commit -m "Added latest tbl_change_request data export to the json."
 
-git push origin master
+REM git push origin master
 
-exit
-
+pause
